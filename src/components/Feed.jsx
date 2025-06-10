ce2f1d73275e9ddccdedaed04c377899614fd6db
Karthik <karthiknair2004@gmail.com>
2025-06-10 18:58:51 +0530

    Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,35 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import UserCard from "./UserCard";
 
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  console.log(feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if(feed) return; // If feed is already present, do not fetch again
+    try {
+    const res = await axios.get(BASE_URL+"/feed",
+    {withCredentials:true});
+    dispatch(addFeed(res.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
   return (
-    <div>Feed</div>
+    feed &&(
+    <div className="flex justify-center my-10">
+      <UserCard user={feed[0]}/>
+    </div>
+    )
   )
 }
 
